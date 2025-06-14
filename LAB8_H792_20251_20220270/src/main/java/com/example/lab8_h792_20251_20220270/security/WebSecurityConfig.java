8f1a7f24122a2e9c038d7b7d6afb3d32c8601721
Christian Flores <167637198+Cjfs2005@users.noreply.github.com>
2025-06-13 21:35:47 -0500

    Creación de credenciales

--- LAB8_H792_20251_20220270/src/main/java/com/example/lab8_h792_20251_20220270/security/WebSecurityConfig.java
+++ LAB8_H792_20251_20220270/src/main/java/com/example/lab8_h792_20251_20220270/security/WebSecurityConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +27,22 @@ public class WebSecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // Sin estado
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(
+                User.withUsername("usuario")
+                        .password(passwordEncoder.encode("20220270"))
+                        .roles("USER")
+                        .build()
+        );
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
